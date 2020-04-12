6328e92c4a98668ed3d106d1716ab9829db63de9
gatchan0807 <yuki.dolphin.koga@gmail.com>
2020-04-13 01:27:42 +0900

    Aboutページでチェック処理を追加

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { route } from "../../constants/routes";
 
 type Props = {};
 
-const About: React.FC<Props> = (props) => {
+const About: React.FC<Props> = () => {
+  const [availableNFC, setAvailableNFC] = useState(false);
+
+  useEffect(() => {
+    if ("NDEFReader" in window) {
+      setAvailableNFC(true);
+    }
+  }, []);
+
   return (
     <>
       <h1>About</h1>
-      <Link to={route.STAMP}>stamp</Link>
+      {availableNFC ? (
+        <Link to={route.STAMP}>打刻ページへ移動する</Link>
+      ) : (
+        <div>このブラウザはWeb NFCに対応していません</div>
+      )}
     </>
   );
 };
