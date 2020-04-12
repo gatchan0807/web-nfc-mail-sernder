bc7b98fb55f6eb19462c6ee03aa22a207d1f531a
gatchan0807 <yuki.dolphin.koga@gmail.com>
2020-04-13 01:06:01 +0900

    BrowserRouterの実装

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { hot } from 'react-hot-loader';
+import { hot } from "react-hot-loader";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 import Stamp from "./pages/Stamp";
 import About from "./pages/About";
+import { route } from "../constants/routes";
 
 type Props = {};
 declare const module: any;
 
 const App: React.FC<Props> = () => {
   return (
-    <section>
-      <Stamp></Stamp>
-      <About></About>
-    </section>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={route.HOME} component={About}></Route>
+        <Route exact path={route.STAMP} component={Stamp}></Route>
+        <Route exact path={route.ABOUT} component={About}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
